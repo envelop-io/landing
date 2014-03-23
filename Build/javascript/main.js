1a16a13072bd72374e2bd91cdf197f3a43bb9ad9
Jerry Yu <hweetty@gmail.com>
2014-03-23 03:10:51 -0400

    + fires rockets

--- Build/javascript/main.js
+++ Build/javascript/main.js
@@ -1,5 +1,12 @@
-function rotateText() {
-	var items = [
+function fireRocket()
+{
+	var t = {
+		main: 	6500,
+		opaque: 300,
+		resize: 300,
+		first: 	3000
+	};
+	var ITEMS = [
 		['updates',		 	'new product features'],
 		['daily digests', 	'your curated design links'],
 		['notifications', 	'your latest blog posts'],
@@ -7,46 +14,86 @@ function rotateText() {
 		['highlights', 		'your favourite recipes'],
 		['daily reports', 	'your users\' activity']
 	];
+	var len = ITEMS.length;
 
-	var options = {
-		interval: 	4500,
-		normal: 	5500
+	var index = 0;
+	var flip1 = $('#flip-first');
+	var flip2 = $('#flip-second');
+	var fuelcells = $('.fuel-cell');
+	var fuel = function (i) {
+		return $('.rocket').find(fuelcells[i]);
 	};
 
-	var flip1 = $('#flip-first'),
-		flip2 = $('#flip-second'),
-		currentIndex = -1,
-		isFirst = true;
+	for (var i = 1; i < len+1; i++) {
+		fuel(i).css({transform: 'translateY(-20px)'});
+		fuel(i).css({opacity: 0});
+	};
 
+	// Make first one comubstable so it can transition
+	fuel(0).addClass('combustable');
 
-	function flipText() {
-
-		if (currentIndex == -1)
-			currentIndex++;
-		else {
-			flip1.css({ opacity: 0 });
-			flip2.css({ opacity: 0 });
-			options.interval = options.normal; // First time is half the time
+	function flipText()
+	{
+		// First run
+		if (flip1.text() == ITEMS[index][0])
+		{
+			index ++;
+			setTimeout(flipText, t.first);
+			return;
 		}
 
-		setTimeout(function () {
+		flip1.css({opacity: 0});
+        flip2.css({opacity: 0});
 
+		setTimeout(function ()
+		{
+			var cf = index==0 ? fuel(len) : fuel(index);
+			cf.addClass('combustable');
+			cf.css({transform: 'translateY(0)'});
+			cf.css({opacity: 1});
+
+			if (index==0)
+			{
+				setTimeout(function () {
+					fuel(0).addClass('combustable');
+					fuel(0).css({transform: 'translateY(0)'});
+					fuel(0).css({opacity: 1});
+					cf.removeClass('combustable');
+				}, 2000);
+			}
+
+			var prev = (index - 1 + len) % len;
 			setTimeout(function () {
-				flip1.css({ opacity: 1 });
-				flip2.css({ opacity: 1 });
-			}, 300);
+				if (prev == 0) prev = len;
+				fuel(prev).removeClass('combustable');
+				fuel(prev).css({transform: 'translateY(' + (-20) +'px)'});
+				fuel(prev).css({opacity: 0});
+			}, 1800);
 
-			flip1.text(items[currentIndex][0]);
-			flip2.text(items[currentIndex][1]);
+			// Change message
+			flip1.text(ITEMS[index][0]);
+			flip2.text(ITEMS[index][1]);
 			$(flip1).parent().width(flip1.width());
 			$(flip2).parent().width(flip2.width());
 
-			currentIndex = (currentIndex + 1) % items.length;
-			setTimeout(flipText, options.interval);
-		}, 300);
-	}
+
+			setTimeout(function () {
+	            flip1.css({opacity: 1});
+	            flip2.css({opacity: 1});
+			}, t.opaque);
+
+			index = (index + 1) % len;
+			setTimeout(flipText, t.main);
+		}, t.resize);
+	};
+
 	
 	flipText();
+
+	setTimeout(function () {
+		$('#rocket').css({display: 'block'});
+		$('#temp-placeholder').remove();
+	}, 200);
 }
 
 $('.feature-one').waypoint(function (direction) {
